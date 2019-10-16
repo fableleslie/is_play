df8de36137e90147be6cc7efb792b50cc18f23fb
Zyizhe <782412901@qq.com>
2019-10-16 17:06:59 +0800

    2019-10-16-4

--- src/pages/detail/activeEvaluation/activeEvaluationUI.jsx
+++ src/pages/detail/activeEvaluation/activeEvaluationUI.jsx
@@ -2,7 +2,8 @@ import React , { Component } from 'react'
 
 import {
     ActiveEvaluationContainer,
-    EvaluationHeader
+    EvaluationHeader,
+    EvaluationContent
 } from './styledactiveEvaluation'
 
 export default class activeEvaluationUI extends Component{
@@ -10,8 +11,22 @@ export default class activeEvaluationUI extends Component{
     return(
       <ActiveEvaluationContainer>
           <EvaluationHeader>
-              
+              <i id="back">&#xe646;</i>
+              <h3>评价详情</h3>
+              <i className="share">&#xe71d;</i>
           </EvaluationHeader>
+          <EvaluationContent>
+            <div className="user-info">
+              <i className="user-head">&#xe545;</i>
+              <div className="name-level">
+                <span className="user-name">用户名</span>
+                <i className="user-level">&#xe501;</i>
+              </div>
+              <span className="create-time">
+                01月01日
+              </span>
+            </div>
+          </EvaluationContent>
       </ActiveEvaluationContainer>
     )
   }
--- src/pages/detail/activeEvaluation/styledactiveEvaluation.js
+++ src/pages/detail/activeEvaluation/styledactiveEvaluation.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
 import border from 'components/styles/border.js'
-import { styledIconOne } from 'components/styles/icons.js'
+import { 
+    styledIconOne,
+    styledIconTwo
+} from 'components/styles/icons.js'
 
 const ActiveEvaluationContainer = styled.div`
     background: #fff;
@@ -9,11 +12,58 @@ const ActiveEvaluationContainer = styled.div`
 
 const EvaluationHeader = styledIconOne(
     styled.header`
-        
+        width: 100%;
+        height: .5rem;
+        background: #FFE60D;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        i{
+            font-family: 'iconfont' !important;
+        }
+        #back{
+            font-size: .15rem;
+            font-weight: bold;
+            margin-left: .2rem;
+        }
+        h3{
+            font-size: .17rem;
+            font-weight: bold;
+        }
+        .share{
+            font-size: .17rem;
+            margin-right: .2rem;
+        }
     `
 )
 
+const EvaluationContent = border({
+    width: "0 0 1px 0",
+    color: "#777",
+    comp: styledIconOne(
+        styledIconTwo(
+            styled.div`
+                i{
+                    font-family: 'iconfont' !important;
+                }
+                .user-info{
+                    width: 100%;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    .user-head{
+                        width: .75rem;
+                        font-size: .42rem;
+                        color: #777;
+                    }
+                }
+            `
+        )
+    )
+})
+
 export {
     ActiveEvaluationContainer,
-    EvaluationHeader
+    EvaluationHeader,
+    EvaluationContent
 }
