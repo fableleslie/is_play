a4ba3af2b98656cdab797d3a5494dee0a4a8e1c6
shuoxiao <15546625691@163.com>
2019-10-25 23:09:35 +0800

    23:09 fyc

--- src/pages/find/guessLike/views/GuessLikeContainer.jsx
+++ src/pages/find/guessLike/views/GuessLikeContainer.jsx
@@ -2,14 +2,28 @@ import React , {Component} from 'react'
 import GuessLike from './GuessLikeUI'
 import {withRouter} from 'react-router-dom'
 class GuessLikeContainer extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            collection:false
+        }
+    }
     render(){
         return <GuessLike
+                state={this.state}
                 jump={this.jump}
+                collectionClick={this.collectionClick}
                 ></GuessLike>
     }
 
     jump = ()=>{
         this.props.history.push('/details')
+    }
+
+    collectionClick = ()=>{
+        this.setState({
+            collection:!this.state.collection
+        })
     }
 }
 export default withRouter(GuessLikeContainer)
